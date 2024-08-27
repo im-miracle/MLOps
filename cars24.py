6d2b086900dff22a70c4fe220d8ddd0a69a6faf6
im-miracle <saivarshith.gangarapu.eee13@itbhu.ac.in>
2024-08-27 11:19:31 -0700

    Initial Setup of streamlit

---
+++ cars24.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import seaborn as sms
+import matplotlib.pyplot as plt
+import streamlit as sl
+
+df = pd.read_csv("/home/miracle/miracle_repo/MLOps/cars24-car-price.csv")
+print(df.head(2))
